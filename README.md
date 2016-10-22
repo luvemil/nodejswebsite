ec215e8f3cc80be6c6b2a670e8b227d3020a4cd0
luvemil <taran.marco@gmail.com>
2016-10-22 12:49:40 +0200

    Use express

---
+++ README.md
@@ -0,0 +1,4 @@
+Run with
+`docker run -p 8080:3000 -it --rm --name my-running-app my-nodejs-app`
+and build with
+`docker build -t my-nodejs-app .`
--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-http.createServer(function(req,res){
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("It's alive!");
-    res.end();
-}).listen(3000);
-
